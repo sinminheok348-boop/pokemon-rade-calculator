38363c76db720841e6e85fd5120e9217fef1d73f
sinminheok348-boop <sinminheok348@gmail.com>
2026-02-25 16:05:44 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-오케이!! 그렇다면 해당 내용을 다른 코드의 수정은 최소화하면서 딱 그부분만 바꿔줘! 이때 마찬가지로 정확히 어디를 어떻게 수정해야할지 전후비교로 구체적이고 명확히 알려줘! 아래는 내 현재 script.js 전체 코드야!
-
 const SHADOW_BG_URL = "./shadow_bg.png";
 let shadowBgImg = new Image();
 shadowBgImg.src = SHADOW_BG_URL;
@@ -1915,3 +1913,4 @@ document.addEventListener("click", (e) => {
     .classList.toggle("hidden", tab !== "noMega");
 
 });
+
